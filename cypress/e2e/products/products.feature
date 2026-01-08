7e4bf5fe3ce224c4703c8bfd027e62021bd5f790
rajiv-sapkota <rajiv.sapkota96@gmail.com>
2026-01-08 15:28:52 +0545

    added cases for products

--- cypress/e2e/products/products.feature
+++ cypress/e2e/products/products.feature
@@ -7,3 +7,23 @@ Feature: Products Page Functionality
   Scenario: i am directed to inventory page after login
     Then i should be on the inventory page
     And the inventory list should be displayed
+    And the hamburger menu icon should be visible
+    And the cart icon should be visible and cart count should not be visible
+    And the filter dropdown should be visible and have default value is visible
+  
+  @positive
+  Scenario: i click on the hamburger menu icon
+    When i click on the hamburger menu icon
+    Then the menu items should be visible 
+
+  @positive
+  Scenario: i click on About link from menu bar
+    When i click on About 
+    Then i should be redirected to About Page
+
+  @positive
+  Scenario: i click on Log Out link from menu bar
+    When i click on Logout link
+    Then confirm log out modal should appear
+
+  
--- cypress/e2e/products/products.js
+++ cypress/e2e/products/products.js
@@ -1,5 +1,6 @@
 import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
-import { assertUtils } from "../utils/commonUtils";
+import { assertUtils } from "../../utils/commonUtils";
+import { clickUtils } from "../../utils/commonUtils";
 
 // precondition: user is logged in
 Given("i am logged in with valid credentials", () => {
@@ -15,3 +16,59 @@ Then("i should be on the inventory page", () => {
 Then("the inventory list should be displayed", () => {
   assertUtils.assertVisibleElement("#inventory_container");
 });
+
+//assertion for hamburger icon visiblility
+Then("the hamburger menu icon should be visible",()=>{
+  assertUtils.assertVisibleElement("#react-burger-menu-btn")
+})
+
+//assertion for cart icon
+Then("the cart icon should be visible and cart count should not be visible",()=>{
+  assertUtils.assertVisibleElement(".shopping_cart_link")
+})
+
+//assertion for filter dropdown visibility and default value
+Then("the filter dropdown should be visible and have default value is visible",()=>{
+  assertUtils.assertVisibleElement(".product_sort_container")
+  assertUtils.assertVisibleText('.active_option',"Name (A to Z)")
+})
+
+//when user clicks on the hamburger menu icon
+When("i click on the hamburger menu icon",()=>{
+   clickUtils.clickElement("#react-burger-menu-btn")
+})
+
+
+// assert menu items to be visible
+Then("the menu items should be visible",()=>{
+  assertUtils.assertVisibleElement(".bm-item-list")
+})
+
+// click on About link from menu bar
+When("i click on About",()=>{
+     clickUtils.clickElement("#react-burger-menu-btn")
+     
+})
+
+//assert i am on About Page
+Then("i should be redirected to About Page",()=>{
+  cy.get("#about_sidebar_link").should("have.attr","href","https://saucelabs.com/")
+})
+
+// click on Logout link from menu bar
+When("i click on Logout link",()=>{
+     clickUtils.clickElement("#react-burger-menu-btn")
+     clickUtils.clickElementByText("Logout")
+})
+
+
+//assert for Confirm Modal of logout
+Then("confirm log out modal should appear",()=>{
+  assertUtils.assertVisibleElement("#logOutConfirmModal")
+})
+
+
+
+
+
+
